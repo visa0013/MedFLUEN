294897511ea46aba0af549e4e294fb01e0c793b9
visa0013 <49792777+visa0013@users.noreply.github.com>
2026-09-25 15:22:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App';
+createRoot(document.getElementById('root')).render(<App/>);
